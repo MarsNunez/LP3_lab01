b29f24f908613b840dfb60f451b5fa205877e741
NASebastian <na.marcelo.sebastian@gmail.com>
2022-08-21 16:44:02 -0500

    Commit rama1

--- Principal.java
+++ Principal.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
-		int vida, rp;
+		int vida, rp, premio;
 		boolean fl = false;
 		vida = 0;
+		premio = 1000;
 		Scanner sc = new Scanner(System.in);
 		do {
 			vida ++;
-			System.out.println("Life(s): " + vida + "\t ¿What's your magic number mate? ");
+			System.out.println("Vida(s): " + vida + "\t ¿Cual es tu numero magico? ");
 			rp = sc.nextInt();
 			if (rp == 1234)
 				fl = true;
+			else 
+				premio -= 100;
 		} while (vida < 3 && !fl );
 		if (fl == true) {
-			System.out.println("You guess it!!!");
+			System.out.println("Adivinaste!, te ganaste un premio.");
+			System.out.println("Te ganaste 1000$, pero por cada error que tuviste se te desconto 100$...");
+			System.out.println("Tu premio total es: " + premio + "$");
 		} else {
-			System.out.println("Ups... You lose.");
+			System.out.println("Ups... Perdiste.");
 		}
 		sc.close();
 	}
